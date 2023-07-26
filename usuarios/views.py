7a95e7002baf7e256fee576fe330bb272293238f
Tati Ramos <tatiramos@ibm.com>
2023-07-26 10:04:22 -0300

    ✨ Iniciando Projeto

---
+++ usuarios/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def login(request):
+    return HttpResponse('login')
+
+def cadastro(request):
+    return HttpResponse('cadastro')
+
